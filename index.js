1e0389b910f7a1ff56a4627cc0868115a7cbe352
fsaiz97 <felixsaiz97@gmail.com>
2022-10-17 15:57:03 +0100

    Replace hardcoded port with .env port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config(); // Loads environment variables
+
 const app = require("./app");
 
-const port = 5000;
+const port = process.env.PORT
 
 app.listen(port, () => {
     console.log(`API listening on port ${port}`)
