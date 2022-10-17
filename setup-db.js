586b2d7ac825558a97a8344e0296b2cefff9a34e
fsaiz97 <felixsaiz97@gmail.com>
2022-10-17 16:33:49 +0100

    Add database connection

---
+++ setup-db.js
@@ -0,0 +1,14 @@
+// Sends the SQL to the database we are connecting to
+const fs = require("fs");
+require("dotenv").config();
+
+// Load the SQL code
+const sql = fs.readFileSync("setup.sql").toString();
+
+// Import our db
+const db = require("./db")
+
+// Run the query - send the SQL code to the database
+db.query(sql)
+    .then(data => console.log("Setup complete", data))
+    .catch(error => console.log(error));
