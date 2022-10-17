9f7950a833d379bd27013fe4b3273efc84161c73
fsaiz97 <felixsaiz97@gmail.com>
2022-10-17 18:09:34 +0100

    Add route logger

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
+const logRoute = require("./route-logger");
 const db = require("./db");
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 function setupMiddleware(app) {
     app.use(express.json());
     app.use(cors());
+    app.use(logRoute);
 }
 
 function addRoutes(app) {
