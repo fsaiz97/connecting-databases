0b7105bdf0ff5786b8a817119661db075c764c62
fsaiz97 <felixsaiz97@gmail.com>
2022-10-17 16:42:49 +0100

    Get wrongs table from database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 
+const db = require("./db");
+
 const app = express();
 
 function setupMiddleware(app) {
@@ -10,6 +12,11 @@ function setupMiddleware(app) {
 
 function addRoutes(app) {
     app.get("/", (req, res) => res.send("Welcome to the Wrongs API: We track injustices!"));
+    
+    app.get("/wrongs", async (req, res) => {
+        const data = await db.query("SELECT * FROM wrong");
+        console.log(data);
+    })
 }
 
 setupMiddleware(app);
